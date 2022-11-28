515c2ead46ac60f538e9b38bc1d37d13a599f8ed
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-11-28 18:59:24 +0100

    changed methods in generateRandomWidth.test

--- src/components/Game/GameWord/tests/generateRandomWidth.test.ts
+++ src/components/Game/GameWord/tests/generateRandomWidth.test.ts
@@ -3,7 +3,7 @@ import { maximumWidth, minimumWidth } from "../GameWord.const";
 
 describe("generating number in range of values declared in GameWord.const.ts", () => {
   test("testing if generated number is within range", () => {
-    expect(generateRandomWidth()).toBeLessThan(maximumWidth);
-    expect(generateRandomWidth()).toBeGreaterThan(minimumWidth - 1);
+    expect(generateRandomWidth()).toBeLessThanOrEqual(maximumWidth);
+    expect(generateRandomWidth()).toBeGreaterThanOrEqual(minimumWidth);
   });
 });
